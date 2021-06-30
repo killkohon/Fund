8d0bd0ea25dd3eb9bed87ffd32886fb943a7b11e
KillkoHon <ydhana@isoftstone.com>
2021-06-30 15:25:31 +0800

    20210630

---
+++ FundCharts/webfundcharts/test.py
@@ -0,0 +1,39 @@
+from flask import Flask, render_template, make_response
+from flask_bootstrap import Bootstrap
+import io
+import numpy as np
+import pandas
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_agg import FigureCanvas
+from wsgiref.simple_server import make_server
+
+flaskapp=Flask(__name__)
+bootstrap = Bootstrap(flaskapp)
+
+@flaskapp.route('/test')
+def chartsTest():
+    return render_template('plot.html', name='test', url='/plot.png')
+
+@flaskapp.route('/plot.png')
+def charttest():
+    fig=plt.figure()
+    subplot=fig.add_subplot(1,1,1)
+    x=np.linspace(1,10,100)
+    y=np.sin(x)
+    subplot.plot(x,y)
+    output=figure2canvas(fig)
+    response = make_response(output.getvalue())
+    response.mimetype = 'image/png'
+    return response
+
+def figure2canvas(fig):
+    canvas = FigureCanvas(fig)
+    output = io.BytesIO()
+    canvas.print_png(output)
+    return output
+
+if __name__ == '__main__':
+    srv = make_server('0.0.0.0', 8080, flaskapp)
+    srv.serve_forever()
