7b827341e8f0f85d59e865b34a01d575352d36ac
KillkoHon <ydhana@isoftstone.com>
2022-07-03 16:44:43 +0800

    update

--- nn2.py
+++ nn2.py
@@ -16,8 +16,8 @@ class neuralnetwork:
 		return sigmoid(x)*(1-sigmoid(x))
 
 	def fit(self,inp,exp):
-		print("input:"+str(inp)+" shape:"+str(inp.shape))
-		print("expect:"+str(exp)+" shape:"+str(exp.shape))
+		print("input:"+str(inp)+" shape:"+str(inp.shape)+" type:"+str(type(inp)))
+		print("expect:"+str(exp)+" shape:"+str(exp.shape)+" type:"+str(type(exp)))
 		if inp.shape[1] != self.layers[0]:
 			print("输入层格式错误.")
 			return
@@ -43,31 +43,54 @@ class neuralnetwork:
 				ohid.append(self.activation(dhid))
 				print("ohid="+str(ohid))
 			dout=ohid[-1]*self.weights[len(self.weights)-1]
-			print("type(dout)="+str(type(dout)))
-			print("dout[0]="+str(dout[0])+" shape:"+str(dout[0].shape))
-			derror=0
-			#e(k)
-			diff=np.mat(np.asarray(dout[0]-exp[i])[0])
-			print("diff="+str(diff)+" type:"+str(type(diff)))
-			print("diff[0]="+str(diff[0]))
-			for k in range(0,len(diff)):
-				derror=derror+np.asarray(diff)[k]**2/2
-			print("derror="+str(derror))
-			print("weights[-1]="+str(self.weights[-1]))
-			dd=self.rate*ohid[-1].T*diff
-			print("dd="+str(dd))
-		print("ohid="+str(ohid))
-		#后向
-		for i in range(0,len(ohid)):
-			print(ohid[i])
-			Imat=np.ones(ohid[i].shape)
-			print(Imat)
-			print("weights["+str(i)+"]:")
-			print(self.weights[i])
-			P1=
-			Param=np.multiply(ohid[i],Imat-ohid[i])
-			print(Param)
+			ohid.append(dout)
+			#print("type(dout)="+str(type(dout)))
+			#print("dout[0]="+str(dout[0])+" shape:"+str(dout[0].shape))
+			print("dout="+str(dout)+" type:"+str(type(dout)))
 
+
+			#后向
+			#-e(d)
+			ned=-np.mat(np.asarray(exp[i]-dout))
+			print("ned="+str(ned)+" type:"+str(type(ned)))
+
+			print("Etotal="+str(ned@ned.T))
+			
+			
+			#从d*d单位阵开始
+			Operators=[]
+			de=ned*np.diagflat(np.ones([1,self.layers[-1]])).T  # 实际就是-ed*I,输出层梯度
+			print("de="+str(de))
+			print("ohid="+str(ohid))
+			Operators.insert(0,ohid[-2].T*de)
+			print("Operators="+str(Operators))
+			#以下计算隐含层梯度
+			for i in range(len(ohid)-2,0,-1):
+				print("==========="+str(i)+"==============")
+				print("ohid["+str(i)+"]="+str(ohid[i]))
+				ta=np.diagflat(np.multiply(ohid[i],np.ones(ohid[i].shape)-ohid[i]))
+				print("ta="+str(ta))
+				print("self.weights["+str(i)+"]="+str(self.weights[i]))
+				print("de="+str(de))
+				de=de*(ta*self.weights[i]).T
+				print("de="+str(de))
+				Operators.insert(0,ohid[i-1].T*de)
+				print("Opertors="+str(Operators))
+			#修正权重
+			for j in range(0,len(self.weights)):
+				self.weights[j]=self.weights[j]+self.rate*Operators[j]
+			print("weights="+str(self.weights))
+	def predict(self,x):
+		x=np.array(x)
+		ohid=[]
+		ohid.append(np.mat(x))
+		for j in range(0,len(self.weights)-1):
+			dhid=ohid[j]*self.weights[j]
+			print("dhid["+str(j)+"]="+str(dhid))
+			ohid.append(self.activation(dhid))
+			print("ohid="+str(ohid))
+		dout=ohid[-1]*self.weights[len(self.weights)-1]
+		print("result="+str(dout))
 
 
 
